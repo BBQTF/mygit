1de4b8cbd4249d953a3ba8f5cd40f8996e330ccb
liutengfei <liutf@riverroad.cn>
2021-05-20 14:31:30 +0800

    初始化项目

---
+++ src/main/java/com/example/gitdemo/Test01.java
@@ -0,0 +1,11 @@
+package com.example.gitdemo;
+
+/**
+ * @author liutf
+ * @date 2021-05-20
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("我是个测试方法， 第一次从本地区远程");
+    }
+}
